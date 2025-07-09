bf80d1f56bd9230c0baffd34aac34077473c562a
arcensyl <dev@arcensyl.me>
2025-07-09 00:09:07 -0500

    Pushed WIP config for the 'beep' command

--- modules/suites/commands.nix
+++ modules/suites/commands.nix
@@ -2,32 +2,47 @@
 
 let
   cfg = config.suites.commands;
+  beepGroup = "beep";
 in {
   options.suites.commands.enable =
     lib.mkEnableOption "the suite for various command-line utilities";
 
-  config.home-manager.users."${vars.master.name}" = lib.mkIf cfg.enable {
-	home.packages = with pkgs; [
-	  wl-clipboard
-	  fastfetch
-	  tealdeer
-	];
-	
-	programs.zoxide.enable = true;
-	
-	programs.eza = {
-	  enable = true;
-	  icons = "auto";
-	};
+  config = lib.mkIf cfg.enable {
+    home-manager.users."${vars.master.name}" = {
+	  home.packages = with pkgs; [
+	    wl-clipboard
+	    fastfetch
+	    tealdeer
+        beep
+	  ];
+	  
+	  programs.zoxide.enable = true;
+	  
+	  programs.eza = {
+	    enable = true;
+	    icons = "auto";
+	  };
 
-    programs.jq.enable = true;
-	programs.fzf.enable = true;
-	programs.bat.enable = true;
+      programs.jq.enable = true;
+	  programs.fzf.enable = true;
+	  programs.bat.enable = true;
 
-	home.shellAliases = {
-	  ls = "eza --oneline";
-	  ll = "eza --oneline --long";
-	  cd = "z";
-	};
+      home.shellAliases = {
+	    ls = "eza --oneline";
+	    ll = "eza --oneline --long";
+	    cd = "z";
+	  };
+    };
+
+    # FIXME: The 'beep' command still doesn't work!
+    # The 'beep' command needs extra set-up to work.
+    # Essentially, we make a 'beep' group, and make our user part of it.
+
+    users.users."${vars.master.name}".extraGroups = [ beepGroup ];
+
+    services.udev.extraRules = ''
+    # Add write access to the PC speaker for the "${beepGroup}" group
+    ACTION=="add", SUBSYSTEM=="input", ATTRS{name}=="PC Speaker", ENV{DEVNAME}!="", RUN+="${pkgs.acl}/bin/setfacl -m g:${beepGroup}:w '$env{DEVNAME}'"
+    '';
   };
 }
